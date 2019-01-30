7055071baab6e806614292ff6b843edc30e62562
s-hayashi1104 <shibahas.has@gmail.com>
2019-01-31 00:38:51 +0900

    リアルタイム文字起こし機能を追加

--- src/components/CreateMinute.vue
+++ src/components/CreateMinute.vue
@@ -5,12 +5,10 @@
       <button @click="logout">Logout</button>
     </div>
     <div id="Minute">
-      <button @click="startListening">Start Listening</button>
-      <form>
+      <button @click="startSpeech">{{ recognitionText }}</button>
         Minute：<br>
-        <textarea rows="1000" cols="100" v-model="minute"></textarea><br>
+        <textarea v-model="minute"/><br>
         <button @click="strage">Submit this minute</button>
-      </form>
     </div>
   </div>
 </template>
@@ -22,7 +20,10 @@ export default {
   name: 'CreateMinute',
   data: function () {
     return {
-      minute: ''
+      minute: '',
+      recognition: new webkitSpeechRecognition(), // eslint-disable-line 
+      recognitionText: 'Start Speech',
+      speechIsValid: true
     }
   },
   methods: {
@@ -30,13 +31,45 @@ export default {
       const content = await api.createMinute(this.username, this.minute)
       if (content) {
         alert('success')
-        this.$router.push('/userpage')
+        this.$router.push('/user')
       } else {
         alert('Registration failed')
       }
     },
     logout: function () {
       this.$router.push('/')
+    },
+    startSpeech: function () {
+      if (!this.speechIsValid) { return }
+      this.recognition.start()
+    },
+    stop: function () {
+      this.speechIsValid = false
+      this.startSpeech()
+    }
+  },
+  created: function () {
+    this.recognition.onstart = () => {
+      this.recognitionText = 'Listenning'
+    }
+    this.recognition.onend = () => {
+      this.recognitionText = 'Start Speech'
+    }
+    this.recognition.onresult = (event) => {
+      this.recognition.continuous = true // マイク使用許可を一回だけにする
+      this.recognition.interimResults = true // 中間結果を返す
+      // 再帰後もインスタンスが生きててonresultイベントが発生するので認識一時停止時もすぐ関数を抜ける処理を入れる
+      if (!this.speechIsValid) { return }
+      // 音声認識の結果は（interimResults == true なら）リアルタイムに認識候補結果を返してくる
+      const results = event.results
+      for (let i = event.resultIndex; i < results.length; i++) {
+        if (results[i].isFinal) {
+          this.minute = results[i][0].transcript
+          this.startSpeech()
+        } else {
+          this.minute = results[i][0].transcript
+        }
+      }
     }
   }
 }
