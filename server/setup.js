b1c25f1b3d385f51fdb9375b10a65339da21f53a
s-hayashi1104 <shibahas.has@gmail.com>
2019-02-13 00:18:06 +0900

    登録処理修正

--- server/setup.js
+++ server/setup.js
@@ -1,35 +1,52 @@
 const userRoute = require('./user-route')
+const firebase = require('firebase')
 const admin = require('firebase-admin')
 const serviceAccount = require('../automatic-creation-of-minutes-firebase-adminsdk-dd99r-ec74771db5.json')
 
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyDI9F4uZ3SzNGOO1HtLT-BNm_1VCLUUKNU',
+  authDomain: 'automatic-creation-of-minutes.firebaseapp.com',
+  databaseURL: 'https://automatic-creation-of-minutes.firebaseio.com',
+  projectId: 'automatic-creation-of-minutes',
+  storageBucket: 'automatic-creation-of-minutes.appspot.com',
+  messagingSenderId: '695781313773'
+}
+firebase.initializeApp(config)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://automatic-creation-of-minutes.firebaseio.com'
 })
 
-const db = admin.database()
-const ref = db.ref('users')
+const db = firebase.firestore()
 
 const setup = function (app) {
   app.post('/api/signup', async (req, res) => {
-    ref.set({
-      id: '1',
-      name: req.body.username,
-      password: req.body.password
-    }).then(function () {
-      res.json(req.body.username)
+    await firebase.auth().createUserWithEmailAndPassword(
+      req.body.username, req.body.password
+    ).then(user => {
+      console.log(user)
+      db.collection('users').doc('7ffX0WTIbVyjfIWbzttp').set({
+        capital: true,
+        id: user.uid,
+        name: user.email,
+        password: req.body.password
+      })
+      res.json(user)
     })
-      .catch(function (err) {
+      .catch(err => {
         console.error('Error writing document: ', err)
       })
   })
   app.post('/api/signin', async (req, res) => {
-    try {
-      res.json({})
-    } catch (err) {
-      console.error(err)
-      res.status(500)
-    }
+    await firebase.auth().signInWithEmailAndPassword(
+      req.body.username, req.body.password
+    ).then(user => {
+      res.json(user)
+    })
+      .catch(err => {
+        console.error('Auth error:', err)
+      })
   })
 
   app.use('/api/user', userRoute)
