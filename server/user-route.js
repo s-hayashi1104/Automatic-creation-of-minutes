91b4e01784cd5245723d31256ec7c2d61e84a8a1
s-hayashi1104 <shibahas.has@gmail.com>
2019-02-14 23:16:01 +0900

    サーバー側にTokenの検証処理をするよう改修

--- server/user-route.js
+++ server/user-route.js
@@ -1,5 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('firebase-admin')
+const serviceAccount = require('../automatic-creation-of-minutes-firebase-adminsdk-dd99r-ec74771db5.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://automatic-creation-of-minutes.firebaseio.com'
+})
+
+// Authentification Filter
+router.use(function (req, res, next) {
+  // get token from body:token or query:token of Http Header:x-access-token
+  const token = req.body.token || req.query.token || req.headers['x-access-token']
+  // validate token
+  if (!token) {
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    })
+  }
+  // idToken comes from the client app (shown above)
+  admin.auth().verifyIdToken(token).then(decodedToken => {
+    let uid = decodedToken.uid
+    next()
+  }).catch(err => {
+    console.error(err)
+  })
+})
 
 router.get('/:userId/minutes', async (req, res) => {
   try {
