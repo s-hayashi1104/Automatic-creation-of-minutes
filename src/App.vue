928edfc7ecf47f4fc7a813cc5f8e76c6b1c60aae
s-hayashi1104 <shibahas.has@gmail.com>
2019-02-28 12:10:55 +0900

    ヘッダー、フッターの追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-    <div class="contents">
-      <div class="header">
-        <h2>Automatic-creation-of-minutes</h2>
-      </div>
-      <router-view/>
-    </div>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-title>Automatic-creation-of-minutes</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <router-view/>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
   </div>
 </template>
 
